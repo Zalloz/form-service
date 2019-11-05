086aa6f8b9de3c3f9168148b653daa68092a4b3a
NickGatti <nick.gatti@gmail.com>
2019-11-04 17:41:00 -0700

    crud endpoints working

--- server/server.js
+++ server/server.js
@@ -56,7 +56,6 @@ http.createServer(function (req, res) {
                             "Access-Control-Allow-Headers": "X-Requested-With"
                         })
                         const reactString = ReactDOMServer.renderToString(<App agents={agents} />)
-                        console.log(reactString)
                         res.end(reactString, 'utf-8')
                     }
                 })
@@ -74,9 +73,65 @@ http.createServer(function (req, res) {
             })
         }
 
-    } else {
-        res.write('Error!');
-        res.end();
+    } else if (req.method === 'POST' && req.url === '/') {
+        let data = ''
+        req.on('data', function (chunk) {
+            data += chunk;
+        });
+        req.on('end', function () {
+            req.rawBody = data;
+            if (data && data.indexOf('{') > -1) {
+                req.body = JSON.parse(data);
+            }
+            data = req.body
+            postgres.query(`INSERT INTO agents(agent_name,recent_sales,phone,agent_type,average_stars,num_ratings,agent_photo) VALUES('${data.agent_name}',${data.recent_sales},'${data.phone}','${data.agent_type}',${data.average_stars},${data.num_ratings},'${data.agent_photo}')`, (err, agentInsert) => {
+                if (err) {
+                    console.log('Insert query error')
+                    console.log(err)
+                } else {
+                    res.end()
+                }
+            })
+        });
+    } else if (req.method === 'PUT' && req.url === '/') {
+        let data = ''
+        req.on('data', function (chunk) {
+            data += chunk;
+        });
+        req.on('end', function () {
+            req.rawBody = data;
+            if (data && data.indexOf('{') > -1) {
+                req.body = JSON.parse(data);
+            }
+            data = req.body
+            postgres.query(`UPDATE agents SET agent_name = '${data.agent_name}', recent_sales = ${data.recent_sales}, phone = '${data.phone}', agent_type = '${data.agent_type}', average_stars = ${data.average_stars}, num_ratings = ${data.num_ratings}, agent_photo = '${data.agent_photo}'  WHERE id = ${data.id}`, (err, updateAgent) => {
+                if (err) {
+                    console.log('Update query error')
+                    console.log(err)
+                } else {
+                    res.end()
+                }
+            })
+        });
+    } else if (req.method === 'DELETE' && req.url === '/') {
+        let data = ''
+        req.on('data', function (chunk) {
+            data += chunk;
+        });
+        req.on('end', function () {
+            req.rawBody = data;
+            if (data && data.indexOf('{') > -1) {
+                req.body = JSON.parse(data);
+            }
+            data = req.body
+            postgres.query(`DELETE FROM agents WHERE id = ${data.id}`, (err, deleteAgent) => {
+                if (err) {
+                    console.log('Delete query error')
+                } else {
+                    res.end()
+                }
+            })
+        });
     }
 }).listen(8081, function () {
     // console.log('Server started on port 8081');
