af3425033eed1039d8f508112e1619d14f11a2e5
NickGatti <nick.gatti@gmail.com>
2019-11-07 11:41:48 -0700

    smaller schema

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -6,11 +6,11 @@ CREATE DATABASE formservice;
 
 CREATE TABLE agents (
   id SERIAL PRIMARY KEY,
-  agent_name varchar(50) NOT NULL,
+  agent_name varchar(25) NOT NULL,
   recent_sales int NOT NULL,
-  phone varchar(20) NOT NULL,
-  agent_type varchar(20) NOT NULL,
+  phone varchar(12) NOT NULL,
+  agent_type varchar(7) NOT NULL,
   average_stars int NOT NULL,
   num_ratings int NOT NULL,
-  agent_photo varchar(120) NOT NULL
+  agent_photo varchar(62) NOT NULL
 );
