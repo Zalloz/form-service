4008fb8b58828ab1266cfb26b88d786374eadd81
NickGatti <nick.gatti@gmail.com>
2019-11-11 15:19:59 -0700

    wut

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ function getAgent(agent, cb) {
                         //Oops!
                         cb(err, null);
                     } else {
-                        console.log(res.rows[0])
+                        console.log('row', res.rows[0])
                         cb(null, res.rows[0]);
                         redisClient.hset(agent.toString(), agent.toString(), JSON.stringify(res.rows[0]), (err, success) => {
                             if (err) {
