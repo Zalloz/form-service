e299acaa83374376ce0017529540ca20ad964b2f
NickGatti <nick.gatti@gmail.com>
2019-11-12 13:07:06 -0700

    loader io verify change

--- server/server.js
+++ server/server.js
@@ -122,13 +122,14 @@ function getAgent(agent, cb) {
 
 http.createServer(function (req, res) {
     if (req.method === 'GET') {
-        // if (req.url === `/loaderio-2a8a167d68cd7c32d8cbd0375cea696f`) {
-        //     let verifyPath = path.join(__dirname, `loaderio-2a8a167d68cd7c32d8cbd0375cea696f`);
-        //     fs.readFile(verifyPath, (err, verifyFile) => {
-        //         res.end('loaderio-2a8a167d68cd7c32d8cbd0375cea696f', 'utf-8');
-        //         return;
-        //     });
-        // };
+        if (req.url === `/loaderio-2a8a167d68cd7c32d8cbd0375cea696f`) {
+            res.end('loaderio-2a8a167d68cd7c32d8cbd0375cea696f', 'utf-8');
+            return
+            // let verifyPath = path.join(__dirname, `loaderio-2a8a167d68cd7c32d8cbd0375cea696f`);
+            // fs.readFile(verifyPath, (err, verifyFile) => {
+            //     return;
+            // });
+        };
         let contentType = 'text/html';
         let extension = path.extname(req.url);
         if (extension === '.js') {
