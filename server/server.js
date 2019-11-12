b93afe8cdd518df0e1af97154e1eb9ef946c8ba8
NickGatti <nick.gatti@gmail.com>
2019-11-12 13:18:29 -0700

    loader io verify change

--- server/server.js
+++ server/server.js
@@ -122,7 +122,6 @@ function getAgent(agent, cb) {
 
 http.createServer(function (req, res) {
     if (req.method === 'GET') {
-        console.log(req.url)
         if (req.url === `/loaderio-2a8a167d68cd7c32d8cbd0375cea696f/`) {
             res.end('loaderio-2a8a167d68cd7c32d8cbd0375cea696f', 'utf-8');
             return;
