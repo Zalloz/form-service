0e824ac8ecc2398d4086a6835883aba8d7ba2605
NickGatti <nick.gatti@gmail.com>
2019-11-11 17:17:01 -0700

    wut

--- server/database/populateRedis.js
+++ server/database/populateRedis.js
@@ -14,23 +14,27 @@ const agentTypes = ['listing', 'premier'];
 
 const populate = async () => {
     for (let i = 7500000; i <= 10000000; i++) {
-        const agent_name = faker.name.firstName() + ' ' + faker.name.lastName();
-        const recent_sales = faker.random.number({ min: 0, max: 30 });
-        const phone = faker.phone.phoneNumber('###-###-####');
-        const agent_type = agentTypes[faker.random.number({ min: 0, max: 1 })];
-        const average_stars = faker.random.number({ min: 0, max: 5 });
-        const num_ratings = faker.random.number({ min: 0, max: 500 });
-        const agent_photo = `https://s3-us-west-2.amazonaws.com/agents-zallo/Realtor${faker.random.number({ min: 1, max: 100 })}.jpg`;
-        const obj = {
-            agent_name,
-            recent_sales,
-            phone,
-            agent_type,
-            average_stars,
-            num_ratings,
-            agent_photo
+        let hGET = await new Promise(resolve => redisClient.hget(`${i}`, `${i}`, resolve))
+        if (hget === null) {
+            console.log('got here', i)
+            const agent_name = faker.name.firstName() + ' ' + faker.name.lastName();
+            const recent_sales = faker.random.number({ min: 0, max: 30 });
+            const phone = faker.phone.phoneNumber('###-###-####');
+            const agent_type = agentTypes[faker.random.number({ min: 0, max: 1 })];
+            const average_stars = faker.random.number({ min: 0, max: 5 });
+            const num_ratings = faker.random.number({ min: 0, max: 500 });
+            const agent_photo = `https://s3-us-west-2.amazonaws.com/agents-zallo/Realtor${faker.random.number({ min: 1, max: 100 })}.jpg`;
+            const obj = {
+                agent_name,
+                recent_sales,
+                phone,
+                agent_type,
+                average_stars,
+                num_ratings,
+                agent_photo
+            }
+            await new Promise(resolve => redisClient.hset(`${i}`, `${i}`, JSON.stringify(obj), resolve));
         }
-        await new Promise(resolve => redisClient.hset(`${i}`, `${i}`, JSON.stringify(obj), resolve));
     }
 }
 populate()
