9f2865281b4976f915981e47c7fe34a1d99b363a
NickGatti <nick.gatti@gmail.com>
2019-11-15 17:47:42 -0700

    final

---
+++ proxy02.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i "Form-Proxy.pem" ec2-user@ec2-18-217-37-162.us-east-2.compute.amazonaws.com
---
+++ proxy03.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i "Form-Proxy.pem" ec2-user@ec2-18-222-195-208.us-east-2.compute.amazonaws.com
